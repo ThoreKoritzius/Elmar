e078682073ff94d245ec77313d731a814d475794
Thore Koritzius <thorekoritzius@outlook.de>
2024-01-06 14:58:57 +0100

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';  // Import HeaderComponent
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
